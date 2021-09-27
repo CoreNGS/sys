3149772f97354791ed6428d959535ea29f1ff6e4
Kai Lyons <kailyons@pop-os.localdomain>
2021-09-26 18:12:52 -0600

    Add kernel source

---
+++ compat/linuxkpi/common/include/linux/inetdevice.h
@@ -0,0 +1,6 @@
+#ifndef	_LINUX_INETDEVICE_H_
+#define	_LINUX_INETDEVICE_H_
+
+#include <linux/netdevice.h>
+
+#endif	/* _LINUX_INETDEVICE_H_ */
---
+++ modules/zfs/zfs_gitrev.h
@@ -0,0 +1,5 @@
+/*
+ * $FreeBSD$
+ */
+
+#define ZFS_META_GITREV "zfs-2.1.99-453-g4a1195ca50"
